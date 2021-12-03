fb51c28117a750a3a88e37e3bd71e67f2d077516
Gilbert Kim <gbertkim@gmail.com>
2021-12-03 14:01:29 -0800

    ui changes

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const projectPre = [
   {
     img: `<img src="./img/jubo.png" id="jubo" alt="jubo app screenshot" class="fullHeight">`,
     title: `JUBO`, 
-    description: `Digital Church Bulletin created to save time, paper, and ink. React.js frontend and Node/PostgreSQL/Express backend`, 
+    description: `Digital church bulletin created to save time, paper, and ink.`, 
+    code: 'ReactJS/Node/PostgreSQL/Express',
     github: "https://github.com/gbertkim/jubo", 
     web: "https://jubo.vercel.app/",
     short: 'digital bulletin'
@@ -13,7 +14,8 @@ const projectPre = [
   {
     img: `<img src="./img/pooper.png" id="pooper" alt="pooper app screenshot" class="fullHeight">`,
     title: `POOPER`, 
-    description: `Web app created to find clean restrooms around you. React.js frontend and Node/Express backend`, 
+    description: `Find clean restrooms around you.`,
+    code: 'ReactJS/Node/PostgreSQL/Express', 
     github: "https://github.com/gbertkim/pooper", 
     web: "https://pooper-gbertkim.vercel.app/",
     short: 'bathroom finder'
@@ -22,11 +24,11 @@ const projectPre = [
   {
     img: `<img src="./img/RRN.png" id="RRN" alt="RRN app screenshot" class="fullHeight">`,
     title: `RRN`, 
-    description: `Track Run River North as they go on their spring music tour. JavaScript, HTML, CSS, Instagram API, and Google Maps API.`, 
+    description: `Track RRN on their spring tour.`,
+    code: 'JavaScript/HTML/CSS/Instagram & Google Maps API.', 
     github: "https://github.com/gbertkim/RRN", 
     web: "https://gbertkim.github.io/RRN/",
     short: 'band tracker'
-
   }
 ]
 const bgColors = {
@@ -43,50 +45,22 @@ const loadPreview = function(){
     $('#preview').append(`
       <div class="appD" id="appD${i}">
       <div class="projInfo">
-        <h3 class='projNumber'>${`${projectPre[i].title} - ${projectPre[i].short}`}</h3>
+        <h3 class='projTitle'>${projectPre[i].title}</h3>
+        <p class='projDesc'>${projectPre[i].description}
+        <p class='projCode'>${projectPre[i].code}</p>
       </div>
         <div class='imageCont'>
           <a class="previewLinks" data-value="${i}">${projectPre[i].img}</a>
         </div>
-        <div class='descOverlay'>
-          <div class='descContainer'>
-            <button class='cancelButt'>
-              <img src='./img/cancel-icon.png' alt='cancel icon' />
-            </button>
-            <p class='descP'>
-              ${projectPre[i].description}
-            </p>
-          </div>
-        </div>
-          <button class='openDescButt'>
-            <img src='./img/information-icon.png' alt='information icon' />
-        </button>
-
         <div class='projLinks'>
-          <a class='projGit' role="link" target="_blank" href="${projectPre[i].github}"><img class='projIcons' src='./img/github1.png' alt='github icon' /></a>
-          <a class='projGit' role="link" target="_blank" href="${projectPre[i].web}"><img class='projIcons' src='./img/view-icon.png' alt='github icon' /></a>
+        <a class='projGit' role="link" target="_blank" href="${projectPre[i].github}">View Live</a>
+        <a class='projGit' role="link" target="_blank" href="${projectPre[i].web}">View Code</a>
         </div>
       </div>
     `)
     $(`#appD${i}`).css("background-color", `${bgColors[i+1]}`);
   }
 }
-const overlayFunctions = () => {
-  $('.descOverlay').click((e) => {
-    e.preventDefault();
-    $(e.target).parents('.descOverlay').fadeOut();
-  });
-  $('.openDescButt').click((e)=> {
-    e.preventDefault();
-    $(e.target).parent().siblings('.descOverlay').fadeIn();
-    e.stopPropagation();
-  });
-  $('.previewLinks').click((e) => {
-    $(e.target).parent().parent().siblings('.descOverlay').fadeIn();
-    e.stopPropagation();
-  });
-}
-
 const hoverPhoto = () => {
   $('#yourName').click((e)=> { 
     $('.banner').fadeIn(100);
@@ -155,9 +129,7 @@ $(document).ready(function(){
   loadPreview();
   $('.banner').css({'display':'none'});
   $('#fries').css({'display':'none'});
-  $('.descOverlay').hide();
   navigationFunctions();
   hoverPhoto();
   hoverAbout();
-  overlayFunctions();
 });
